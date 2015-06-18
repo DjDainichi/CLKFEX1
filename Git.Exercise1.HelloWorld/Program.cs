25d988efc3d97ebf2c7adf1c5929e2b7c4317ec5
Dainichi <dainichi@me.com>
2015-06-18 03:50:15 -0400

    First commit of this Exercise

---
+++ Git.Exercise1.HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Exercise1.HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// The purpose of this program is to provide git the framwork necessary for creating a tracked repository
+        /// This simple program is to be written daily and taken through the various stages of a developers daily workflow
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World welcome to git CLKF Excercise 1 !");
+            Console.ReadLine();
+        }
+    }
+}
